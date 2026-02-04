8e90a8293d8f5d6bf4f301592eadfc1d1a30eb84
mhmdmetwally <mhmdedak@gmail.com>
2026-02-04 16:16:45 +0200

    verify token

---
+++ middleware/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt=require('jsonwebtoken');
+const app_error = require('../utils/AppError');
+const JWT_SECRET=process.env.JWT_SECRET;
+const http_status_text=require('../utils/http_status_text');
+const verify_token=(req,res,next)=>{
+    const auth_header=req.headers["Authorization"] || req.headers["authorization"];
+    if(!auth_header)
+    {
+        const err=new app_error();
+        const message = "required token";
+        err.create(message,401,http_status_text.ERORR);
+        return next(err);
+    }
+    const token = auth_header.split(' ')[1];
+    try {
+        const decoded_token= jwt.verify(token,JWT_SECRET);    
+        next();
+    } catch (error) {
+        const err=new app_error();
+        const message = "invalid token";
+        err.create(message,401,http_status_text.ERORR);
+        next(err);
+    }
+}
+module.exports = verify_token ;
